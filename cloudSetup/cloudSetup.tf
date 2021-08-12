69533b6f6723d1d9f7a75c9eb871071f42a7452c
Jakob Braun <jakob.braun@posteo.de>
2021-08-12 07:14:16 -0300

    added quick-check workflow

--- cloudSetup/cloudSetup.tf
+++ cloudSetup/cloudSetup.tf
@@ -1,6 +1,3 @@
-# This file use you ssh key stored in ~/.ssh/id_rsa.pub. Please make sure that you generated this file via ssh-keygen -m PEM.
-# Keys generated without the -m PEM will lead to login fails.
-
 provider "aws" {
   region = "eu-central-1"
   profile = "c1-admin"
@@ -12,4 +9,5 @@ module "exasol_setup" {
   deputy = var.deputy
   datanode_count = 1
   project = "ETSA"
+  license = "./docker-db.lic"
 }
