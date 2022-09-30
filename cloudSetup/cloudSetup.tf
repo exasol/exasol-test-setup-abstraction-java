e9423952c49aa3a32f7f0f8486dca46ca5602cee
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-09-30 13:49:23 +0200

    Upgrade to latest terraform module

--- cloudSetup/cloudSetup.tf
+++ cloudSetup/cloudSetup.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 module "exasol_setup" {
   source         = "exasol/exasol-test-setup/aws"
-  version        = "1.0.0"
+  version        = "1.1.0"
   owner          = var.owner
   deputy         = var.deputy
   datanode_count = 1
