1d7384d826694f934eb93f13f1db464c2fb182d1
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-01-18 10:52:40 +0100

    Fix typos in comment

--- src/test/java/com/exasol/exasoltestsetup/standalone/StandaloneExasolTestSetupIT.java
+++ src/test/java/com/exasol/exasoltestsetup/standalone/StandaloneExasolTestSetupIT.java
@@ -9,9 +9,9 @@ import com.exasol.exasoltestsetup.ExasolTestSetupTestBase;
 
 /**
  * This test requires a running Exasol cluster. Please start the cluster first by running <i>terraform apply</i> in
- * <i>cloudSetup/</i>. Terraform will the write the credentials into <i>setEnv.sh</i>. By executing this script you can
+ * <i>cloudSetup/</i>. Terraform will then write the credentials into <i>setEnv.sh</i>. By executing this script you can
  * setup the environment variables. (Tip: run <i>. ./setEnv.sh</i> to set the variable in a shell). Next start your IDE
- * or tests in the same shell to that this test can read the configuration from the environment variables. In the CI
+ * or tests in the same shell so that this test can read the configuration from the environment variables. In the CI
  * this process is done automatically.
  */
 @Tag("cloudOnly")
