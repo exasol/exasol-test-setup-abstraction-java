72c25d76917ac0e4eecf8f635418c2f60520bba0
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-04-12 15:09:49 +0200

    #55: Upgrade to latest Exasol version

--- src/main/java/com/exasol/exasoltestsetup/testcontainers/ExasolTestcontainerTestSetup.java
+++ src/main/java/com/exasol/exasoltestsetup/testcontainers/ExasolTestcontainerTestSetup.java
@@ -22,7 +22,7 @@ import com.jcraft.jsch.*;
  */
 public class ExasolTestcontainerTestSetup implements ExasolTestSetup {
     private static final int SSH_PORT = 22;
-    private final ExasolContainer<? extends ExasolContainer<?>> exasolContainer = new ExasolContainer<>("7.1.15")
+    private final ExasolContainer<? extends ExasolContainer<?>> exasolContainer = new ExasolContainer<>("7.1.19")
             .withReuse(true);
     private final SshConnection sshConnection;
     private final KeyPair keyPair;
